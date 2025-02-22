671fe7f98f750b021070f80e8cd7da05f4f62962
Artem <skickoartem74@gmail.com>
2025-02-22 22:34:21 +0100

    ^_^

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-function getPreparedMovies(movies, { querry }) {
+function getPreparedMovies(movies, { query: query }) {
   let preparedMovies = movies;
-  const normalizedQuerry = querry.trim().toLowerCase();
+  const normalizedQuery = query.trim().toLowerCase();
 
-  if (normalizedQuerry) {
+  if (normalizedQuery) {
     preparedMovies = preparedMovies.filter(
       movie =>
-        movie.title.toLowerCase().includes(normalizedQuerry) ||
-        movie.description.toLowerCase().includes(normalizedQuerry),
+        movie.title.toLowerCase().includes(normalizedQuery) ||
+        movie.description.toLowerCase().includes(normalizedQuery),
     );
   }
 
@@ -20,8 +20,8 @@ function getPreparedMovies(movies, { querry }) {
 }
 
 export const App = () => {
-  const [querry, setQuerry] = useState('');
-  const visibleMovies = getPreparedMovies(moviesFromServer, { querry });
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovies(moviesFromServer, { query: query });
 
   return (
     <div className="page">
@@ -35,12 +35,12 @@ export const App = () => {
 
             <div className="control">
               <input
-                value={querry}
+                value={query}
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                onChange={event => setQuerry(event.target.value)}
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
